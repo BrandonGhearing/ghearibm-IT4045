f80aee49571f10be1d9fd64aeddf481ae70891a1
Administrator <Administrator@DESKTOP-099UQRG>
2018-09-30 19:39:24 -0400

    Add Javadoc, add default / endpoint

---
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -0,0 +1,22 @@
+package com.plantplaces;
+
+import org.springframework.stereotype.Controller;
+// couldnt get the web annotation to import
+
+@Controller
+public class PlantPlacesController {
+	
+	/**
+	 * Handle the /start endpoint.
+	 * @return
+	 */
+	@RequestMapping("/start")
+	public string start() {
+		return "start";
+	}
+	
+	@RequestMapping("/")
+	public string index() {
+		return "start";
+	}
+}
